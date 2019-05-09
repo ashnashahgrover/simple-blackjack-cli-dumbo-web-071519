32ea99ba4f24bf9eb45ce6c4d4279043c53053b8
Ashna Shah-Grover <as19@williams.edu>
2019-05-09 19:51:44 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -44,7 +44,7 @@ def hit?(card_total)
 end
 
 def invalid_command
-  puts "Please enter a valid command". 
+  puts "Please enter a valid command"
 end
 
 #####################################################
